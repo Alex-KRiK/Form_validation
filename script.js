d111020490289a5a8e179b7096c10c3a7176ed9c
Alexander Gvozd <alexandr.gvozd@gmail.com>
2017-12-20 23:30:07 +0200

    add check for empty fields

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+var form = document.querySelector('.formWithValidation');
 
+var validateBtn = form.querySelector('.validateBtn');
+var from = document.querySelector('.from');
+var password = document.querySelector('.password');
+var passwordConfirmation = document.querySelector('.passwordConfirmation');
+var where = document.querySelector('.where');
+var message = document.querySelector('.message');
+
+var fields = document.querySelectorAll('.field');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    for(var i = 0; i < fields.length; i++) {
+        if (!fields[i].value) {
+            var error = document.createElement('div');
+            error.className = 'error';
+            error.style.color = 'red';
+            error.innerHTML = 'cannot be blank';
+            fields[i].parentElement.insertBefore(error, fields[i]);
+        }
+    }
+});
